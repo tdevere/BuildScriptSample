10b558ddeeccfc380ce6c5a2b2b907910ecc41d8
tdevere <tdevere@microsoft.com>
2022-01-21 20:05:03 -0600

    updated script

--- 53577900042258/53577900042258.Android/appcenter-post-clone.sh
+++ 53577900042258/53577900042258.Android/appcenter-post-clone.sh
@@ -1,10 +1,11 @@
 ﻿#!/usr/bin/env bash
 echo "Android Sample"
 echo "appcenter-post-clone.sh - https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/"
-
-echo "Debug Step: search for .SLN and .CSPROJ files"
+echo "Debug Step: Begin"
+echo "Debug Step: search for .SLN"
 find . -name '*.sln' -type f -print0 
-find . -name '*.csproj' -type f -print0 
-
-echo "Nuget Test"
+echo "Debug Step: search for .CSPROJ files"
+find . -name '*.csproj' -type f -print0
+echo "Debug Step: Nuget Test"
 nuget
+echo "Debug Step: End"
