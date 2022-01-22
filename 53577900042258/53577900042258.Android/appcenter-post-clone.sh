429f1bd000107134a6e43ecc0a504da57d2259f8
tdevere <tdevere@microsoft.com>
2022-01-21 19:58:34 -0600

    nuget test and search for .sln and .csproj

--- 53577900042258/53577900042258.Android/appcenter-post-clone.sh
+++ 53577900042258/53577900042258.Android/appcenter-post-clone.sh
@@ -1,3 +1,10 @@
 ﻿#!/usr/bin/env bash
 echo "Android Sample"
 echo "appcenter-post-clone.sh - https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/"
+
+echo "Debug Step: search for .SLN and .CSPROJ files"
+find . -name '*.sln' -type f -print0 
+find . -name '*.csproj' -type f -print0 
+
+echo "Nuget Test"
+nuget
